68b854ff6e5d23203e750a14d0598af4a5561714
KayTee1 <kha.truong@tuni.fi>
2023-10-11 18:22:50 +0300

    app crash after pressing confirm

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
 import pyautogui
 from time import sleep
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import ttk
 
-class AutoChampGUI:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Auto-Champ-Select")
-        self.root.geometry("400x400")
-        title_label = ttk.Label(master = self.root, text = "Automatic Champ Select", font = "calibri 24 bold")
-        title_label.pack()
+label_list = []
+def inChampSelect(to_ban, to_pick):
+    accepted = False
+    banned = False
+    chose = False
+    sleep_duration = 0.5
 
-        inputTxt = tk.Text(self.root,
-				height = 5,
-				width = 20)
-        inputTxt.pack()
-        acceptButton = tk.Button(self.root,
-						text = "Print",
-						command = printInput)
-        acceptButton.pack()
+    #searching for accept button
+    while findImage("imgs/accepted.png") is None and accepted == False:
+        sleep(sleep_duration)
+        try:
+            #hovered accept: when someone dodges, the mouse is still on the accept button
+            #the accept button glows when its hovered
+            click("imgs/accept.png") or click("imgs/hovered_accept.png")
+        except:
+            print("cant find accept")
+    accepted = True
+    print("In Champ select")
+
+    #banning and picking phase
+
+    banning_phase(to_ban, banned, sleep_duration)
+    picking_phase(to_pick, to_ban, chose, sleep_duration)
+
+def makeInputFields(app, entry_vars):
+    input_frame_ban = tk.Frame(app)
+    entry_ban = ttk.Entry(input_frame_ban, textvariable=entry_vars[0])
+    entry_pick = ttk.Entry(input_frame_ban, textvariable=entry_vars[1])
+    button = ttk.Button(input_frame_ban, text='Confirm',
+                        command=lambda: processInputs(app, entry_vars))
+    entry_ban.pack(side='left', padx=10)
+    entry_pick.pack(side='left', padx=10)
+    button.pack(side='left')
+    input_frame_ban.pack(pady=10)
 
 
-    def run(self):
-        self.root.mainloop()
+def makeLabel(app, string):
+    champToBanLabel = ttk.Label(app, text=string, font="calibri 18")
+    champToBanLabel.pack()
+    label_list.append(champToBanLabel)
+
+
+def processInputs(app, entry_vars):
+    for label in label_list:
+        label.destroy()
+
+    label_list.clear()
+
+    to_ban = entry_vars[0].get()
+    to_pick = entry_vars[1].get()
+    print("Champ To Ban:", to_ban)
+    print("Champ To Pick:", to_pick)
+
+    if to_pick != "" and to_ban != "":
+        makeLabel(app, "In Queue")
+        print("HI")
+        sleep(1)
+
+        inChampSelect(to_ban, to_pick)
+    else:
+        makeLabel(app, "Invalid Input!")
 
 
 
@@ -92,36 +133,26 @@ def picking_phase(to_select, to_ban, chose, sleep_duration):
     chose = True
 
 def main():
-    app = AutoChampGUI()
-    app.run()
+    app = tk.Tk()
+    app.title("Auto-Champ-Select")
+    app.geometry('400x300')
+
+    title_label = ttk.Label(
+        app, text="Automatic Champ Select", font="calibri 24 bold")
+    title_label.pack()
+
+    to_ban = tk.StringVar()
+    to_pick = tk.StringVar()
+
+    input_label = ttk.Label(
+        app, text="Champ To Ban and Champ To Pick:", font="calibri 18")
+    input_label.pack()
+
+    makeInputFields(app, [to_ban, to_pick])
+
+    app.mainloop()
 
 
-def oldMain():
 
-    accepted = False
-    banned = False
-    chose = False
-    sleep_duration = 0.5
-
-    to_ban = input("what champion to ban?: ")
-    to_select= input("what champion to play?: ")
-    print("In Queue")
-
-    #searching for accept button
-    while findImage("imgs/accepted.png") is None and accepted == False:
-        sleep(sleep_duration)
-        try:
-            #hovered accept: when someone dodges, the mouse is still on the accept button
-            #the accept button glows when its hovered
-            click("imgs/accept.png") or click("imgs/hovered_accept.png")
-        except:
-            print("cant find accept")
-    accepted = True
-    print("In Champ select")
-
-    #banning and picking phase
-
-    banning_phase(to_ban, banned, sleep_duration)
-    picking_phase(to_select, to_ban, chose, sleep_duration)
 if __name__ == "__main__":
     main()
